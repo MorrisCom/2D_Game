d68e99f3512c62dbe2e3a175f8faca6e487e7f26
MorrisCom <57181186+MorrisCom@users.noreply.github.com>
2019-11-13 21:48:13 +0800

    事件初步使用

--- Unity_First/Assets/Script/Ground.cs
+++ Unity_First/Assets/Script/Ground.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
-   [Header("這個是速度哦")][Range(0.5f,5)]
+   [Header("這個是速度哦")][Range(0.1f,3)]
  public float speed = 1.5f;
+
+    public Transform ground;
+
+    private void Update()
+    {
+        ground.Translate(-speed, 0, 0);
+    }
+
 }
---
+++ Unity_First/Assets/Script/LearnAPI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log ("正常喔");
+        Debug.LogWarning("打錯了");
+        
+
+        Debug.Log(Mathf.PI);
+        //靜態方法 static properties
+        Debug.Log(Random.value);
+        //靜態方法 static method
+        Debug.Log(Random.Range(1, 10));
+    }
+    
+}
