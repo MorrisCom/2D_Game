5ab9d7856ca05f5cf26301976c2ba42c428191ce
MorrisCom <57181186+MorrisCom@users.noreply.github.com>
2019-11-22 21:30:19 +0800

    運算式的介紹

---
+++ Unity_First/Assets/Script/LearnIf.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnIf : MonoBehaviour
+{
+    public bool open;
+
+    public int score;
+
+    public int combo;
+
+    private void Start()
+    {
+        if (true)
+        {
+            print("哈囉");
+        }
+
+        if (open)
+        {
+            print("開門");
+        }
+        else
+        {
+            print("關門");
+        }
+
+        if (score >= 60)
+        {
+            print("及格");
+        }
+        else if (score >= 40)
+        {
+            print("補考");
+        }
+        else
+        {
+            print("不及格");
+        }
+
+        if (combo < 50)
+        {
+            print("攻擊");
+        }
+        else if (combo < 100)
+        {
+            print("攻擊x2");
+        }
+        else if (combo < 150)
+        {
+            print("攻擊X5");
+        }
+        else
+        {
+            print("攻擊*10");
+        }
+
+    }
+        
+    
+    
+    
+}
---
+++ Unity_First/Assets/Script/LearnOperator.cs
@@ -0,0 +1,53 @@
+﻿
+using UnityEngine;
+
+public class LearnOperator : MonoBehaviour
+{
+    public int a = 55, b = 6;
+    public int c = 3, d = 9;
+    private void Start()
+    {
+        #region
+        print(a + b);
+        print(a - b);
+        print(a * b);
+        print(a / b);
+        print(a % b);
+
+        a = a + 1;
+        print(a);
+        print(a++);//先輸出a再遞增
+        print(++a);//先遞增再輸出
+        print(b--);
+        print(--b);
+        #endregion
+        print(b += 10);
+        #region 比較運算子
+        print(c > d);  //true 大於
+        print(c <d);   //flase  小於      
+        print(c >= d);  //true      大於等於    
+        print(c <= d);  //flase     小於等於
+        print(c == d);  //flase     等於
+        print(c != d);  //true     不等於
+        #endregion
+        #region  邏輯運算子
+        //&& 並且 shift+7
+        //只要有一個false結果就是false
+        print(true && true);
+        print(true && false);
+        print(false && true);
+        print(false && true);
+        // || 或者 shift+\
+        // 只要有一個 true 結果就是 true
+        print(true || true); //t
+        print(true || false); //t
+        print(false || true); //t
+        print(false || false); //f
+        // 相反!
+        print(!true);
+        #endregion
+
+
+    }
+
+}
